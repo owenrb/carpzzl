1161109d0e838698852fdbcf2b33eb92e2b28a9b
owen.bringino <owen.bringino@metrobank.com.ph>
2022-09-11 18:25:26 +0800

    add text to speech feature

--- src/screen/auth.screen.js
+++ src/screen/auth.screen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -8,12 +8,22 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import Tts from 'react-native-tts';
 import {faker} from '@faker-js/faker';
 
 const createPassword = () => faker.word.adjective() + ' ' + faker.animal.type();
+Tts.addEventListener('tts-finish', event => console.log('finish', event));
 
 const AuthScreen = () => {
   const [password, setPassword] = useState(createPassword());
+
+  useEffect(() => {
+    if (password) {
+      Tts.getInitStatus().then(() => {
+        Tts.speak('To login, press mic and say, ' + password);
+      });
+    }
+  }, [password]);
 
   return (
     <SafeAreaView>
